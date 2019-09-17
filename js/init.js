e6de25b0d667c252a2fd74859ac7b6f68430255d
Chris Swenor <cswenor@gmail.com>
2019-09-17 11:59:53 -0400

    More Cleanup

--- js/init.js
+++ js/init.js
@@ -85,14 +85,7 @@ jQuery(document).ready(function($) {
 	$('input, textarea').placeholder()
 
 
-/*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
-   setTimeout( function() {
 
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	   
-   }, 100);
 
 /*----------------------------------------------------*/
 /* Final Countdown Settings
@@ -110,62 +103,6 @@ jQuery(document).ready(function($) {
    });  
 
 
-/*----------------------------------------------------*/
-/*	gmaps settings
-------------------------------------------------------*/
-
-   var map;
-
-   // main directions
-   map = new GMaps({
-      el: '#map', 
-      lat: 14.549072, 
-      lng: 121.046958, 
-      zoom: 14, 
-      zoomControl : true,
-      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, 
-      panControl : false, 
-      scrollwheel: false
-   });
-
-   map.drawOverlay({
-	   lat: map.getCenter().lat(),
-	   lng: map.getCenter().lng(),
-	   content: '<i class="fa fa-map-marker"></i>',
-	   verticalAlign: 'top',
-	   horizontalAlign: 'center'
-	});
-
-   // The styles below present a simplified map. 
-   // If you would like to use a normal coloured map, then please remove or comment the code below, from lines 128 to 148.
-	var mapStyles = [
-	   {
-	      featureType: "road",
-	      elementType: "geometry",
-	      stylers: [{
-	         lightness: 100
-	      }, {
-	         visibility: "simplified"
-	      }]
-	   }, {
-	      featureType: "road",
-	      elementType: "labels",
-	      stylers: [{
-	         visibility: "off"
-	      }]
-	   }
-	];
-	
-	map.setOptions({
-	   styles: mapStyles
-	});
-
-   // map.addMarker({ 
- 		// lat: map.getCenter().lat(),
-	  //  lng: map.getCenter().lng(), 
- 		// title: '5th Avenue',
-   //  	infoWindow: { content: '<p>You can add your address 1 here</p>' } 
-   // });
 
 
 /*----------------------------------------------------*/
